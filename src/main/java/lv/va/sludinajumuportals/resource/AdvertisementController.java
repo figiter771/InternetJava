8041c21a529d366f2104f67573982b31e6141bd4
figiter771 <figiter771@gmail.com>
2018-04-24 15:03:59 +0300

    AdvertisementController fix

--- src/main/java/lv/va/sludinajumuportals/resource/AdvertisementController.java
+++ src/main/java/lv/va/sludinajumuportals/resource/AdvertisementController.java
@@ -8,20 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 
 public class AdvertisementController { 
-        //This is normal html code that sets up a Get from /status
-        public String getHtml = "<form method=\"get\" action=\"status\" class=\"inline\">" +
-
-            "  <button type=\"submit\" class=\"link-button\">\n" +
-
-            "    CheckStatus\n" +
-
-            "  </button>\n" +
-
-            "</form>";
+      
     @GetMapping(value = "/") // The address /
     @ResponseBody
     public String testEndpoint() {
-        return ("<h1>Hello darkness my old friend!<h1> <br>")+getHtml;
+        return ("<h1>Hello darkness my old friend!<h1> <br>");
     }
 
     @GetMapping(value = "/hey") // The address /hey
